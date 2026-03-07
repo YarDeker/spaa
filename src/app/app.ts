cfe819c2891216a6288977da8a27900e9bc079bb
YarDeker <s.yaros.vit@gmail.com>
2026-03-07 12:20:57 +0200

    styles for sport-card sport-list components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './core/header/header';
 import { Footer } from './core/footer/footer';
+import { SportList } from "./features/products/sport-list/sport-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, SportList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
