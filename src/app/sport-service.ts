42c0423dd89340e2b3a66198caa1064b42f8cb3b
YarDeker <s.yaros.vit@gmail.com>
2026-03-25 10:08:17 +0200

    add data encapsulation

--- src/app/sport-service.ts
+++ src/app/sport-service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { SPORT_ACTIVITIES } from './shared/mock-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SportService {
+  private items = SPORT_ACTIVITIES;
   
+  getAll() {
+    return [...this.items];
+  }
+
+  getById (id: number) {
+    return this.items.find(item => item.id === id) || null;
+  }
 }
