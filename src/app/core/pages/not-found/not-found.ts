c40b57f9cbe808614942598975e600ab0c1759f4
YarDeker <s.yaros.vit@gmail.com>
2026-03-29 09:15:25 +0300

    add not found page

---
+++ src/app/core/pages/not-found/not-found.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'sport-not-found',
+  imports: [RouterLink],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.css',
+})
+export class NotFound {
+
+}
